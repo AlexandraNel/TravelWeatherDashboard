c59ad9fa528a5bc0da70155a05c60fe1ea0bf911
Lex <info@alexandranel.com>
2024-01-16 16:38:24 +1100

    completed task, can be improved upon

--- Assets/script.js
+++ Assets/script.js
@@ -6,6 +6,12 @@ var currentTemp = document.getElementById("currentTemp");
 var currentWind = document.getElementById("currentWind");
 var currentHumidity = document.getElementById("currentHumidity");
 var locationHeader = document.getElementById("locationHeader");
+var forecastDay = document.querySelectorAll(".forecast-day"); //get all the list items for forecast
+var rainboxImg = document.createElement("img");
+rainboxImg.src = "./Assets/favicon.ico";
+var daysIndex = 0; //global counter to iterate trhough data for forecasts
+var leftColumn = document.getElementById("search-bar");
+
 // API key required for access
 var APIKey = "1469a0fc17dbb9a6e1b4cc2b019e6b33";
 
@@ -24,7 +30,10 @@ function inputAlert(input) {
 function getApiCurrent() {
 
     //calling the function that checks if the city, country has been entered correctly, if not- show an alert
-    if (inputAlert(cityName.value) === false) {
+    //if true call my save to local storage function called 'savedSearch
+    if (inputAlert(cityName.value)) {
+        savedSearch(cityName.value);
+    } else {
         window.alert("Please write the name of your CITY, separated by a COMMA (,), followed by your COUNTRY name")
         return; //writing return will stop the api from returning the data if it is written incorrectly
     }
@@ -46,10 +55,12 @@ function getApiCurrent() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&appid=" + APIKey + "&units=metric";
 
     fetch(queryURL)
+        // handles if the incorrect city is entered ie. it does not exist
+
         .then(function (response) {
             if (response.ok) {
                 return response.json();
-                // handles if the incorrect city is entered ie. it does not exist
+
             } else { alert("Please enter a valid city"); }
 
         })
@@ -63,15 +74,20 @@ function getApiCurrent() {
             var lon = data.coord.lon;
 
             // grabbing the date which is in unix and converting it for use in a variable
-            var unixTimestamp= data.dt;
-            var dateConvert= new Date(unixTimestamp*1000);
-            var date = dateConvert.toLocaleDateString("en-GB");
-         
+            var unixTimestamp = data.dt;
+            var dateConvert = new Date(unixTimestamp * 1000);
+            var date = dateConvert.toLocaleDateString("en-GB", {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+
             // here we are filling the text content of our elements (declared at the top of our script) to the data we have 
             // recieved from the API. We create a new span element in order to style it differently and dynamically using 
             //bootstrap class as well as replace the title 'Your Location' with the current area displayed
 
-            locationHeader.innerHTML = (city + ", " + country + ". " + date);
+            locationHeader.innerHTML = (city + ", " + country + "<br>" + date);
 
             var spanTemp = document.createElement("span"); //dynamically create a span element for the response data
             spanTemp.textContent = data.main.temp + " \u00B0C"; //unicode for the degree symbol added here to the text Content recieved
@@ -92,53 +108,149 @@ function getApiCurrent() {
             currentHumidity.appendChild(spanHumidity);
 
             // Because the 5 day forecast requires longitude and latitude location data, we have captured it within variables
-            //These variables are now being passed to our getApiForecast function
-            getApiForecast(lat, lon);
+            //These variables are now being passed to our getApiForecast function incl city and country data
+            getApiForecast(lat, lon, city, country);
         })
 };
 
-function getApiForecast(lat, lon) {
+function getApiForecast(lat, lon, city, country) {
 
     //concatenating our user input, API key and required parameters to query url, using lon and lat data from previous funtion
     var daysQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric";
 
     fetch(daysQueryUrl)
         .then(function (response) {
-            return response.json();
+            if (!response.ok) {
+                throw new Error("No response, please try again")
+            } return response.json()
         })
+
         .then(function (data) {
             console.log("data = ", data); //console logging data with title 'data=' can be removed- for my own benefit
 
+
             // response returns 40 objects within an array (8 results for 5days).
-            // I want the 12pm forecast of each day, loop at [2] which is 12pm today. 
-            // Increments in 8's so i have i have writte i+=8 to get each days 12 noon.
-            
-            for (var i = 2; i < data.list.length; i += 8) {
+            // I want the 12pm forecast of each day, loop at [3] which is 12pm today. 
+            // Increments at every 8th entry to get 12 noon for each day
+            for (var i = 3; i < data.list.length; i += 8) {
+                //save the data list arrays in a variable
+                var forecast = data.list[i];
+                console.log(forecast); //checking if i actually get 12pm
 
+                //given an abbreviated method for the date conversion
+                //putting all my required data into variables
+                var forecastDate = new Date(forecast.dt * 1000).toLocaleDateString("en-GB", {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                });
+                var forecastTemp = forecast.main.temp;
+                var forecastWind = forecast.wind.speed;
+                var forecastHumidity = forecast.main.humidity;
+                var forecastLocation = city + " " + country;
 
-
-                //create element
-                //assign list.ie temp --- list.ie wind [i]
+                createElements(forecastDate, forecastTemp, forecastWind, forecastHumidity, forecastLocation);
             }
-    //     }
+        })
+        //advised to catch any errors from the API retrieval
+        .catch(function (error) {
+            console.error("Error in fetch", error);
+            alert.error("Error with forecast, please try again");
+        })
+};
 
-    //         var spanTemp = document.createElement("span"); //dynamically create a span element for the response data
-    // spanTemp.textContent = data.main.temp + " \u00B0C"; //unicode for the degree symbol added here to the text Content recieved
-    // spanTemp.classList.add("text-primary"); //adding a boostrap class to our new text content for the aesthetic value
-    // currentTemp.innerHTML = "Temp: "; //making sure the HTML is clear with only 'Temp:' inside it
-    // currentTemp.appendChild(spanTemp); //adding our new shiny, colourful span element containing the temp data from the API
+//function to use on each piece of data parsed from previous function 
+function createElements(forecastDate, forecastTemp, forecastWind, forecastHumidity, forecastLocation) {
 
-    // //             // currentTemp.textContent= `Temp: ${data.main.temp} \u00B0C`
-    // //             // currentWind.textContent= `Wind: ${data.wind.speed} KPH`
-    // //             // currentHumidity.textContent= `Humidity: ${data.main.humidity} %`
+    //if statement using global index counter variable
+    //if the counter is less than the amount of forecast list items then use this function
+    if (daysIndex < forecastDay.length) {
 
+        //list item is our list item class by how many there are
+        var listItem = forecastDay[daysIndex]
 
+        //clear existing content
+        listItem.innerHTML = "";
 
+        // create a header
+        var forecastHeader = document.createElement("h6");
+        forecastHeader.innerHTML = forecastDate + "<br>" + forecastLocation;
+        forecastHeader.classList.add("text-primary");
+        listItem.appendChild(forecastHeader);
 
+        var temp = document.createElement("p");
+        temp.textContent = "Temp: " + forecastTemp + " \u00B0C";
+        listItem.appendChild(temp);
 
-})
+        var wind = document.createElement("p");
+        wind.textContent = "Wind: " + forecastWind + " KPH";
+        listItem.appendChild(wind);
+
+        var humidity = document.createElement("p");
+        humidity.textContent = "Humidity: " + forecastHumidity + " %";
+        listItem.appendChild(humidity);
+
+        daysIndex++;
+    }
+};
+
+///this function is having the user input search values passed into it within the getApiCurrent
+//the parameter has been named similarly for legibility
+function savedSearch(cityName) {
+
+    //retrieve existing data if in local storage, if not create an empty array
+    //push new data to the array we either retrieved or made during parse
+    //set the local storage data 
+    var searches = JSON.parse(localStorage.getItem('citySearched')) || [];
+    searches.push(cityName);
+    localStorage.setItem('citySearched', JSON.stringify(searches));
+
+    savedSearchButtons(searches);
+
 }
 
+//calling the buttons on page load- again help with this one as I coudlnt do it myself!
+function callButtons(){
+    var savedSearches = JSON.parse(localStorage.getItem('citySearched')) || [];
+    savedSearchButtons(savedSearches);
+}
+
+// Call this function to load saved searches when the page loads
+callButtons();
+
+
+//had a LOT of help with this function as I was very very stuck
+function savedSearchButtons(searches) {
+    searches.forEach(function (citySearched) {
+        // Check if a button for this city already exists with query selector data-city, if it DOESNT, then keep going
+        if (!document.querySelector(`button[data-city='${citySearched}']`)) {
+
+            var memoryButton = document.createElement("button");//create button
+            memoryButton.textContent = citySearched; // Set button text to the saved city name
+            memoryButton.classList.add("search-btn", "btn", "btn-info", "fw-bold"); // Adding Bootstrap classes
+             // This sets attricbutes of the city name and 'data-city', this allows us to differentiate repeats
+            memoryButton.setAttribute('data-city', citySearched);
+
+            // Adding an event listener to each button
+            memoryButton.addEventListener('click', function() {
+                cityName.value = citySearched; // Set the input field to the saved city name
+                getApiCurrent(); // Trigger the search
+            });
+
+            leftColumn.appendChild(memoryButton); // Append the button
+        }
+    });
+}
+
+ 
 
 // event listener on click run function 
 search.addEventListener("click", getApiCurrent)
+
+//local storage
+// -if input is valid save as a button append underneath serach input area
+//save the search parameter to local storage
+//on click return search parameter results from local storage
+
+
--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # TravelWeatherDashboard
 This web app will allow you to view the 5 day weather forecast of any city
+
+## Description
+This web application will allow you to view teh weather of any city in the world.
+It will give you todays temperature, wind, humidity as well as the next 5 days.
+Your searches will be saved, and can be accessed easily via a button. 
+
+
+## Technologies
+
+- Git Hub
+- Html
+- Css
+- Javascript
+- Evan Hahn's ISO 3166 country list
+- Open Weather Map Api 2.5
+- Bootstrap v5.3.2
+- Google Api Fonts
+
+This application was an activity in allowing me to explore the application server side api's
+
+## Visuals
+
+![Weather Page Visual](./Assets/Visual.JPG)
+
+## Installation
+
+Website has been deployed and is available from this link:
+
+[Travel Weather Dashboard](https://alexandranel.github.io/WorkdaySchedule/)
+
+Full repository can be accessed here:
+
+[Git Repository](https://github.com/AlexandraNel/WorkdaySchedule)
+
+``````
+To view repo contents please navigate to the above link there you will find
+- assets folder
+    - gif
+    - License
+    - script.js
+    - Style.css
+- README.md
+- index.html
+
+``````
+
+## Usage
+
+This website was constructed in an effort to improve jquery and dayjs knowledge
+Its usage is of a simple 9-5, daily scheduler.
+
+## Support
+
+If you come into any issues with this web application please contact
+info@alexandranel.com
+
+## Roadmap
+
+Moving forward the following improvements can be made
+
+- Design can be imrpoved by extending the css stylesheets
+- Daily tasks can be saved to a server in order to retrieve yearly progress reports
+
+## Authors and Acknowledgment
+
+This website was made under the guidance and with assistance from
+- the team at MONASH UNI full Stack Dev Bootcamp
+- Instructor Chee Ho Tai
+- TA Pranita Shrestha
+- Tutor Weng Fei Fung 
+
+## Licence
+MIT License
